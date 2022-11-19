22da4b23b5a09c9d8715e73c78d67425588b0703
Mitch McCullough <mitchmccullough@gmail.com>
2022-11-18 19:26:00 -0600

    chapter 5

--- levelup/settings.py
+++ levelup/settings.py
@@ -44,12 +44,8 @@ INSTALLED_APPS = [
 
 # THIS IS NEW
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 # THIS IS NEW
--- levelup/urls.py
+++ levelup/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from levelupapi.views import register_user, check_user
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Requests to http://localhost:8000/register will be routed to the register_user function
+    path('register', register_user),
+    # Requests to http://localhost:8000/checkuser will be routed to the login_user function
+    path('checkuser', check_user),
+    path('admin/', admin.site.urls)
 ]
